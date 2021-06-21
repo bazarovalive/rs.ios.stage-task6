517da691fae7f19b1354fb9bdb4950a451072c9c
Olga Stash <olya.stash@yandex.ru>
2021-06-21 22:56:46 +0300

    init: add minimum of task

--- Swift_Task6/ios.stage-task/Exercises/Exercise-1/CoronaClass.swift
+++ Swift_Task6/ios.stage-task/Exercises/Exercise-1/CoronaClass.swift
@@ -2,17 +2,36 @@ import Foundation
 
 class CoronaClass {
  
-     var seats = [Int]()
-
+    var seats = [Int]() { didSet { seats.sort() } }
+    let n: Int
+    
      init(n: Int) {
- 
+        if n > 0 && n <= 100 {
+            self.n = n
+        } else {
+            self.n = 0
+        }
      }
      
      func seat() -> Int {
-        -1
+        var numOfSeat = 0
+        if seats.isEmpty {
+            numOfSeat = 0
+            seats.append(0)
+        } else if seats.count == n {
+            numOfSeat = 0
+        } else {
+            
+            
+        }
+        return numOfSeat
      }
      
      func leave(_ p: Int) {
-         
+        if seats.contains(p) {
+            if let index = seats.firstIndex(of: p) {
+                seats.remove(at: index)
+            }
+        }
      }
 }
--- Swift_Task6/ios.stage-task/Exercises/Exercise-2/Deck.swift
+++ Swift_Task6/ios.stage-task/Exercises/Exercise-2/Deck.swift
@@ -28,26 +28,48 @@ extension Deck {
 
     init(with type: DeckType) {
         self.type = type
+//        let suits = Suit.allCases
+//        let values = Value.allCases
+//        cards = createDeck(suits: suits, values: values)
     }
 
     public func createDeck(suits:[Suit], values:[Value]) -> [Card] {
-        []
+        var deck = [Card]()
+               for suit in suits {
+                   for value in values {
+                       deck.append(Card(suit: suit, value: value))
+                   }
+               }
+               return deck
     }
 
-    public func shuffle() {
-
+    public mutating func shuffle() {
+        cards.shuffle()
     }
 
-    public func defineTrump() {
-
+    public mutating func defineTrump() {
+        if self.cards.count > 0 {
+            self.trump = self.cards.first!.suit
+            self.setTrumpCards(for: self.trump!)
+        }
     }
 
-    public func initialCardsDealForPlayers(players: [Player]) {
+    public mutating func initialCardsDealForPlayers(players: [Player]) {
+            for player in players {
+                player.hand = .init()
+                for _ in 1...6 {
+                    guard let card = cards.popLast() else {return}
+                    player.hand?.append(card)
+                }
+            }
+        }
 
-    }
-
-    public func setTrumpCards(for suit:Suit) {
-
-    }
+    public mutating func setTrumpCards(for suit:Suit) {
+            for (index, card) in cards.enumerated() {
+                if card.suit == suit {
+                    cards[index].isTrump = true
+                }
+            }
+        }
 }
 
